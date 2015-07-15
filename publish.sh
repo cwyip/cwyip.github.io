4c470298b03e47bfe7dba2495d46bf6feb0dac83
Janice Leung <jjw1707@gmail.com>
2015-07-15 15:29:56 +0800

    publish.sh script.

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+message="Site generated on $( date +%Y-%m-%d--%T )"
+ 
+jekyll build && \
+  cd _site && \
+  git add . && \
+  git commit -am $message && \
+  git push origin master && \
+  cd .. && \
+  echo "Successfully built and pushed to GitHub."
