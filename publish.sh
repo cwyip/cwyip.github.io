120b261e02ff42e531ddb4560534412748df0c98
Janice Leung <jjw1707@gmail.com>
2015-07-15 16:06:54 +0800

    adding the publish script.

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ message="Site generated on $( date +%Y-%m-%d--%T )"
 jekyll build && \
   cd _site && \
   git add . && \
-  git commit -am $message && \
+  git commit -am "$message" && \
   git push origin master && \
   cd .. && \
   echo "Successfully built and pushed to GitHub."
