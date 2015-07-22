07fdee663c87a896fbee4fcf2c4609c48a06b1f0
Janice Leung <jjw1707@gmail.com>
2015-07-23 00:49:05 +0800

    accordion effect.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 
 toggle_nav = function(nav_id) {
     if (nav_id == "desktop_art_sub_nav") {
-        nav = document.getElementById(nav_id);
-        if (nav.className == "second-level hide") {
-            nav.className = "second-level";
-        } else {
-            nav.className = "second-level hide";
-        }
+        $('#desktop_art_sub_nav').slideToggle("fast", 
+             function() {});
+        // nav = document.getElementById(nav_id);
+        // if (nav.className == "second-level hide") {
+        //     nav.className = "second-level";
+        // } else {
+        //     nav.className = "second-level hide";
+        // }
         return false;
     } else {
         return true;
